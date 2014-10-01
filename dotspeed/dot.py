15e2716ae2a7476835fd82d8f1d322b9882d286f
rizar <dimabv@tut.by>
2014-10-01 15:38:28 +0200

    Also GEMM

--- dotspeed/dot.py
+++ dotspeed/dot.py
@@ -27,22 +27,28 @@ def main():
 
     x = TT.matrix('x')
     y = TT.matrix('y')
+    z = TT.matrix('z')
 
     a = nr.normal(size=(n, m), scale=0.01).astype("float32")
     b = nr.normal(size=(n, n), scale=0.01).astype("float32")
 
-    logger.info("Compiling")
-    func1 = theano.function(inputs=[x, y], outputs=[x.dot(y)], name='case1cc')
-    func2 = theano.function(inputs=[x, y], outputs=[x.T.dot(y)], name='case1fc')
-    func3 = theano.function(inputs=[x, y], outputs=[x.dot(y.T)], name='case1cf')
-    func4 = theano.function(inputs=[x, y], outputs=[x.T.dot(y.T)], name='case1ff')
-    func5 = theano.function(inputs=[x, y], outputs=[x.dot(y)], name='case2cc')
-    func6 = theano.function(inputs=[x, y], outputs=[x.T.dot(y)], name='case2fc')
-    func7 = theano.function(inputs=[x, y], outputs=[x.dot(y.T)], name='case2cf')
-    func8 = theano.function(inputs=[x, y], outputs=[x.T.dot(y.T)], name='case2ff')
+    logger.info("Compiling DOTs")
+    func1 = theano.function(inputs=[x, y], outputs=[x.dot(y)], name='dot1cc')
+    func2 = theano.function(inputs=[x, y], outputs=[x.T.dot(y)], name='dot1fc')
+    func3 = theano.function(inputs=[x, y], outputs=[x.dot(y.T)], name='dot1cf')
+    func4 = theano.function(inputs=[x, y], outputs=[x.T.dot(y.T)], name='dot1ff')
+    func5 = theano.function(inputs=[x, y], outputs=[x.dot(y)], name='dot2cc')
+    func6 = theano.function(inputs=[x, y], outputs=[x.T.dot(y)], name='dot2fc')
+    func7 = theano.function(inputs=[x, y], outputs=[x.dot(y.T)], name='dot2cf')
+    func8 = theano.function(inputs=[x, y], outputs=[x.T.dot(y.T)], name='dot2ff')
+    logger.info("Compiling GEMMs")
+    func9 = theano.function(inputs=[x, y, z], outputs=[x.dot(y) + z], name="gemm1cc")
+    func10 = theano.function(inputs=[x, y, z], outputs=[x.T.dot(y) + z], name="gemm1fc")
+    func11 = theano.function(inputs=[x, y, z], outputs=[x.dot(y.T) + z], name="gemm1cf")
+    func12 = theano.function(inputs=[x, y, z], outputs=[x.T.dot(y.T) + z], name="gemm1ff")
 
     logger.info("Run")
-    times = 500
+    times = 50 if theano.config.device == 'cpu' else 500
     for i in range(times):
         func1(a, a.T)
         func2(a.T, a.T)
@@ -52,6 +58,10 @@ def main():
         func6(a, b)
         func7(a.T, b)
         func8(a, b)
+        func9(a, a.T, b)
+        func10(a.T, a.T, b)
+        func11(a, a, b)
+        func12(a.T, a, b)
     logger.info("Finished")
 
 if __name__ == "__main__":
